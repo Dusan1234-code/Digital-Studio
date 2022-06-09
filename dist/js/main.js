b1d75c81c4e1bc6aafba8088ef0b243eb9ad3f62
Dusan1234-code <dusangacesa727@gmail.com>
2022-06-09 15:24:21 +0200

    header done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,18 @@
-console.log('test');
+const hamburger = document.querySelector(".js-hamburger");
+const overlay = document.querySelector(".js-overlay");
+const close = document.querySelector(".js-close");
+const body = document.querySelector(".body");
+
+
+
+
+
+hamburger.addEventListener("click",()=>{
+    body.classList.add("scroll");
+    overlay.classList.toggle("header__overlay--active");
+})
+
+close.addEventListener("click",()=>{
+    body.classList.remove("scroll");
+    overlay.classList.remove("header__overlay--active");
+})
