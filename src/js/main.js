86fe4762dc599b9ae031789b0a2e977d59212afa
Dusan1234-code <dusangacesa727@gmail.com>
2022-06-16 16:34:12 +0200

    overlay updated

--- src/js/main.js
+++ src/js/main.js
@@ -4,11 +4,11 @@ const close = document.querySelector(".js-close");
 const body = document.querySelector(".js-body");
 
 hamburger.addEventListener("click",()=>{
-    body.classList.add("scrollDisabled");
+    body.classList.add("body--scrollDisabled");
     overlay.classList.toggle("header__overlay--active");
 })
 
 close.addEventListener("click",()=>{
-    body.classList.remove("scrollDisabled");
+    body.classList.remove("body--scrollDisabled");
     overlay.classList.remove("header__overlay--active");
 })
