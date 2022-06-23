688d3dbcc53d622ad5c4d9f80130c014e2d39582
Dusan1234-code <dusangacesa727@gmail.com>
2022-06-23 16:04:36 +0200

    footer done

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,16 @@ const hamburger = document.querySelector(".js-hamburger");
 const overlay = document.querySelector(".js-overlay");
 const close = document.querySelector(".js-close");
 const body = document.querySelector(".js-body");
+const accordionButtons = document.querySelectorAll(".js-accordion-btn");
+const accWrapper = document.querySelectorAll(".js-acc-wrapper");
 
+// Display overlay menu
 hamburger.addEventListener("click",()=>{
     body.classList.add("body--scrollDisabled");
     overlay.classList.toggle("header__overlay--active");
 })
 
+// Closes overlay menu
 close.addEventListener("click",()=>{
     body.classList.remove("body--scrollDisabled");
     overlay.classList.remove("header__overlay--active");
@@ -22,3 +26,13 @@ const swiper = new Swiper(".swiper", {
       prevEl: ".js-prev",
     },
   });
+
+  // Accordion
+accordionButtons.forEach((btn)=>{
+  btn.addEventListener("click",()=>{
+    let wrapper = btn.parentElement;
+    wrapper.classList.toggle("accordion__wrapper--opens");
+  })
+})
+
+
